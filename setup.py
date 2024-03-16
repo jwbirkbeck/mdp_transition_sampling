0743ccd376552ef482ea9fdf2ed817ed8041747d
jwbirkbeck <29716383+jwbirkbeck@users.noreply.github.com>
2024-03-16 13:41:43 +0000

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mdp_transition_sampling',
+    version='0.0.1',
+    packages=['src'],
+    url='',
+    license='',
+    author='John Birkbeck',
+    author_email='jwgb1n21@soton.ac.uk',
+    description='MDP Transition Sampling'
+)
