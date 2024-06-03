8d5c3d14e9d97a97e203f31972d2ae356eea8337
John B <29716383+jwbirkbeck@users.noreply.github.com>
2024-06-03 12:03:31 +0100

    latest

--- scripts/plots_results/confirmation_review/c3_002_random_vs_reward_based_sampling.py
+++ scripts/plots_results/confirmation_review/c3_002_random_vs_reward_based_sampling.py
@@ -23,10 +23,10 @@ task_sel = results.env_a.unique()
 results = results[results.env_a.isin(task_sel)]
 results = results[results.env_b.isin(task_sel)]
 results = results.reset_index()
-task_sel = ['dial-turn-v2']
+task_sel = [task_sel[0]]
 
 plt.rcParams.update({'figure.autolayout': True})
-for task in task_sel:
+for task in [task_sel[2]]:
     plotdata = results[results.env_a == task]
     if len(plotdata) != 0:
         ax = plotdata.groupby('env_b').boxplot(column=['rand_dist'], subplots=False, figsize=(9/1.4, 6/1.4))
@@ -40,3 +40,18 @@ for task in task_sel:
         plt.tight_layout()
         # plt.savefig('mdp_distances_smallest_sample_reach.png', dpi=200)
         plt.show()
+
+        ax = plotdata.groupby('env_b').boxplot(column=['rew_dist'], subplots=False, figsize=(9/1.4, 6/1.4))
+        env_b_list = results.env_b.unique().tolist()
+        ax.set_xticks(ticks=range(1, len(env_b_list) + 1), labels=env_b_list)
+        ax.get_xticklabels()[env_b_list.index(task)].set_color('red')
+        plt.xticks(rotation=-45, ha='left', rotation_mode='anchor')
+        plt.ylim(0, 7.0)
+        plt.ylabel("Approx. W1 distance")
+        plt.title("W1 distance approximations, smallest samples")
+        plt.tight_layout()
+        # plt.savefig('mdp_distances_smallest_sample_reach.png', dpi=200)
+        plt.show()
+
+results.groupby(['env_a', 'env_b', 'n_states', 'n_transitions'])[['rand_dist', 'rew_dist']].std().boxplot()
+plt.show()
