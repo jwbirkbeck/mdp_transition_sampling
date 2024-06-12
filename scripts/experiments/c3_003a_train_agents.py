0ad0f8f90e839289f0c90c11b8839f8326e667b7
John Birkbeck <29716383+jwbirkbeck@users.noreply.github.com>
2024-06-12 14:13:48 +0100

    adding time saving to agent training

--- scripts/experiments/c3_003a_train_agents.py
+++ scripts/experiments/c3_003a_train_agents.py
@@ -3,6 +3,7 @@ import os
 import torch
 import numpy as np
 import pandas as pd
+from time import time
 from src.soft_actor_critic.sac_agent_v2 import SACAgentV2
 from src.metaworld.wrapper import MetaWorldWrapper
 from src.utils.consts import task_pool_10
@@ -32,15 +33,19 @@ train_results = pd.DataFrame()
 eval_results = pd.DataFrame()
 for rep in range(n_training_eps):
     for ind, task in enumerate(task_pool_10):
+        start_time = time()
         ep_rew, _ = agents[ind].train_agent()
-        pd_row = pd.DataFrame({'task': [task], 'episode': [rep], 'rewards': [ep_rew]})
+        end_time = time()
+        pd_row = pd.DataFrame({'task': [task], 'episode': [rep], 'rewards': [ep_rew], 'time': [end_time - start_time]})
         train_results = pd.concat((train_results, pd_row))
         train_results.to_csv(os.path.join(results_dir, f'train_{run_index}.csv'), index=False)
         if rep % n_eps_per_checkpoint == 0:
             # Eval agent n_eval_eps times
             for eval_rep in range(n_eval_eps):
+                start_time = time()
                 ep_rew, _ = agents[ind].train_agent(train=False)
-                pd_row = pd.DataFrame({'task': [task], 'episode': [rep], 'rewards': [ep_rew]})
+                end_time = time()
+                pd_row = pd.DataFrame({'task': [task], 'episode': [rep], 'rewards': [ep_rew], 'time': [end_time - start_time]})
                 eval_results = pd.concat((eval_results, pd_row))
             eval_results.to_csv(os.path.join(results_dir, f'eval_{run_index}.csv'), index=False)
             agents[ind].save(save_path=os.path.join(model_dir, f'{task}_model_{run_index}'))
